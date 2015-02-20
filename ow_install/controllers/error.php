836a6d0fe5149bfecb44d9b0df2cdd8585f544ca
Sergey Kambalin <greyexpert@gmail.com>
2015-02-20 17:08:41 +0600

    ow_install removed from the platform

--- ow_install/controllers/error.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class INSTALL_CTRL_Error extends INSTALL_ActionController
-{
-    public function notFound()
-    {
-        $this->redirect(OW::getRouter()->urlForRoute('requirements'));
-    }
-}
--- ow_install/view/controllers/install_finish.php
+++
@@ -1,4 +0,0 @@
-<h1>Security</h1>
-<p>
-    <center>Please remove <b>ow_install/</b> folder and refresh this page.</center>
-</p>
