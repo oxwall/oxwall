a7d047f41e7d98a8ddb58dc29b7d4499122d493a
= <alexermashev@gmail.com>
2015-04-03 15:03:44 +0600

    add a new lang key for welcome widget

--- ow_updates/updates/8570/update.php
+++ ow_updates/updates/8570/update.php
@@ -1,3 +1,2 @@
 <?php
-UPDATE_LanguageService::getInstance()->deleteLangKey('base', 'welcome_widget_content');
 UPDATE_LanguageService::getInstance()->importPrefixFromZip(dirname(__FILE__) . DS . 'langs.zip', 'base');
