aa72a9c21852fecd56d086e33a71ec5a4b67c6cb
Sergey Kambalin <greyexpert@gmail.com>
2015-07-20 15:58:50 +0600

    Widgets refatoring: added reusable settings components

--- ow_system_plugins/base/components/component_frontend_settings.php
+++ ow_system_plugins/base/components/component_frontend_settings.php
@@ -23,14 +23,19 @@
  */
 
 /**
- * Widget Entity Settings
+ * Widget Frontend Settings
  *
  * @author Sergey Kambalin <greyexpert@gmail.com>
  * @package ow_system_plugins.base.components
  * @since 1.0
  */
-class BASE_CMP_ComponentEntitySettings extends BASE_CMP_ComponentFrontendSettings
+class BASE_CMP_ComponentFrontendSettings extends BASE_CMP_ComponentSettings
 {
-    
+    public function __construct($uniqName, $componentSettings = array(), $defaultSettings = array(), $access = null) 
+    {
+        parent::__construct($uniqName, $componentSettings, $defaultSettings, $access);
+        
+        $this->markAsHidden('freeze');
+    }
 }
 
