0654e23f5a68c6d86989b1dfdddf5ef0cfcfdc7f
Sergey Kambalin <greyexpert@gmail.com>
2015-09-09 14:51:46 +0600

    Base install: configs update

--- ow_system_plugins/base/install/configs.php
+++ ow_system_plugins/base/install/configs.php
@@ -26,17 +26,18 @@
 
 OW::getConfig()->addConfig("base", "avatar_big_size", "190", "User avatar width");
 OW::getConfig()->addConfig("base", "avatar_size", "90", "User avatar height");
-OW::getConfig()->addConfig("base", "selectedTheme", "origin", "Selected theme.");
+OW::getConfig()->addConfig("admin", "admin_menu_state", json_encode(array()), null);
+OW::getConfig()->addConfig("base", "selectedTheme", "simplicity", "Selected theme.");
 OW::getConfig()->addConfig("base", "military_time", "1", "Desc");
-OW::getConfig()->addConfig("base", "site_name", "ow-1.7.0", "Site name");
+OW::getConfig()->addConfig("base", "site_name", "", "Site name");
 OW::getConfig()->addConfig("base", "confirm_email", "1", "Confirm email");
 OW::getConfig()->addConfig("base", "user_view_presentation", "table", "User view presentation");
-OW::getConfig()->addConfig("base", "site_tagline", "ow-1.7.0", "Site tagline");
+OW::getConfig()->addConfig("base", "site_tagline", "", "Site tagline");
 OW::getConfig()->addConfig("base", "site_description", "Just another Oxwall site", "Site Description");
 OW::getConfig()->addConfig("base", "site_timezone", "US/Pacific", "Site Timezone");
 OW::getConfig()->addConfig("base", "site_use_relative_time", "1", "Use relative date/time");
-OW::getConfig()->addConfig("base", "display_name_question", "username", "Question used for display name");
-OW::getConfig()->addConfig("base", "site_email", "qwe@mail.com", "Email address from which your users will receive notifications and mass mailing.");
+OW::getConfig()->addConfig("base", "display_name_question", "realname", "Question used for display name");
+OW::getConfig()->addConfig("base", "site_email", "", "Email address from which your users will receive notifications and mass mailing.");
 OW::getConfig()->addConfig("base", "google_analytics", "NULL,NULL", null);
 OW::getConfig()->addConfig("base", "mail_smtp_enabled", null, "Smtp enabled");
 OW::getConfig()->addConfig("base", "date_field_format", "dmy", "Date format");
@@ -54,23 +55,31 @@ OW::getConfig()->addConfig("base", "splash_leave_url", "http://google.com", null
 OW::getConfig()->addConfig("base", "maintenance", null, null);
 OW::getConfig()->addConfig("base", "mandatory_user_approve", null, "mandatory_user_approve");
 OW::getConfig()->addConfig("base", "billing_currency", "USD", "Site currency 3-char code");
+OW::getConfig()->addConfig("base", "site_statistics_disallowed_entity_types", "user-status,avatar-change", null);
 OW::getConfig()->addConfig("base", "tf_max_pic_size", "2.500000", null);
-OW::getConfig()->addConfig("base", "soft_build", "8710", "Current soft version");
+OW::getConfig()->addConfig("base", "soft_build", "9900", "Current soft version");
 OW::getConfig()->addConfig("base", "update_soft", null, "Soft core update flag");
 OW::getConfig()->addConfig("base", "unverify_site_email", null, "Email address from which your users will receive notifications and mass mailing.");
-OW::getConfig()->addConfig("base", "soft_version", "1.7.3", null);
+OW::getConfig()->addConfig("base", "soft_version", "1.8.0", null);
 OW::getConfig()->addConfig("base", "site_installed", null, null);
 OW::getConfig()->addConfig("base", "check_mupdates_ts", null, "Last manual updates check timestamp.");
+OW::getConfig()->addConfig("contact_importer", "yahoo_consumer_key", null, "");
+OW::getConfig()->addConfig("contact_importer", "facebook_app_secret", null, "");
+OW::getConfig()->addConfig("admin", "mass_mailing_timestamp", null, null);
 OW::getConfig()->addConfig("base", "dev_mode", "1", null);
 OW::getConfig()->addConfig("base", "log_file_max_size_mb", "20", null);
 OW::getConfig()->addConfig("base", "attch_file_max_size_mb", "2", null);
-OW::getConfig()->addConfig("base", "attch_ext_list", "[\"txt\",\"doc\",\"docx\",\"sql\",\"csv\",\"xls\",\"ppt\",\"pdf\",\"jpg\",\"jpeg\",\"png\",\"gif\",\"bmp\",\"psd\",\"ai\",\"avi\",\"wmv\",\"mp3\",\"3gp\",\"flv\",\"mkv\",\"mpeg\",\"mpg\",\"swf\",\"zip\",\"gz\",\"tgz\",\"gzip\",\"7z\",\"bzip2\",\"rar\"]", null);
-OW::getConfig()->addConfig("base", "admin_cookie", "turUXYruzErYXEJymA8eBeZy7aWyqYju", null);
+OW::getConfig()->addConfig("base", "attch_ext_list", json_encode(array(
+    "txt","doc","docx","sql","csv","xls","ppt","pdf","jpg","jpeg","png","gif","bmp","psd","ai","avi","wmv","mp3","3gp","flv","mkv","mpeg","mpg","swf","zip","gz","tgz","gzip","7z","bzip2","rar"
+)), null);
+OW::getConfig()->addConfig("base", "admin_cookie", "", null);
 OW::getConfig()->addConfig("base", "disable_mobile_context", null, null);
 OW::getConfig()->addConfig("base", "default_avatar", "[]", "Default avatar");
 OW::getConfig()->addConfig("base", "language_switch_allowed", "1", "Allow users switch languages on site");
 OW::getConfig()->addConfig("base", "rss_loading", null, null);
 OW::getConfig()->addConfig("base", "cron_is_active", "1", "Flag showing if cron script is activated after soft install");
+OW::getConfig()->addConfig("contact_importer", "yahoo_app_id", null, "");
+OW::getConfig()->addConfig("contact_importer", "facebook_api_key", null, "");
 OW::getConfig()->addConfig("base", "users_count_on_page", "30", "Users count on page");
 OW::getConfig()->addConfig("base", "join_display_photo_upload", "display", "Display \'Photo Upload\' field on Join page.");
 OW::getConfig()->addConfig("base", "join_photo_upload_set_required", "1", "Make \'Photo Upload\' a required field on Join Page.");
@@ -78,10 +87,14 @@ OW::getConfig()->addConfig("base", "join_display_terms_of_use", null, "Display \
 OW::getConfig()->addConfig("base", "favicon", "1", null);
 OW::getConfig()->addConfig("base", "html_head_code", null, "Code (meta, css, js) added from admin panel into head section of HTML document.");
 OW::getConfig()->addConfig("base", "html_prebody_code", null, "Code (js) added before \'body\' closing tag.");
+OW::getConfig()->addConfig("contact_importer", "yahoo_consumer_secret", null, "");
 OW::getConfig()->addConfig("base", "tf_user_custom_html_disable", "1", null);
 OW::getConfig()->addConfig("base", "tf_user_rich_media_disable", null, null);
 OW::getConfig()->addConfig("base", "tf_comments_rich_media_disable", null, null);
-OW::getConfig()->addConfig("base", "tf_resource_list", "[\"clipfish.de\",\"youtube.com\",\"google.com\",\"metacafe.com\",\"myspace.com\",\"novamov.com\",\"myvideo.de\"]", null);
+OW::getConfig()->addConfig("base", "tf_resource_list", json_encode(array(
+    "clipfish.de", "youtube.com", "google.com", "metacafe.com", "myspace.com", "novamov.com", "myvideo.de"
+)), null);
+OW::getConfig()->addConfig("contact_importer", "yahoo_domain_verification_file", null, "");
 OW::getConfig()->addConfig("base", "cachedEntitiesPostfix", "53b266e920eba", null);
 OW::getConfig()->addConfig("base", "master_page_theme_info", "[]", null);
 OW::getConfig()->addConfig("base", "user_invites_limit", "50", null);
