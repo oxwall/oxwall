ecf650d319bfe94f7d6c1c735a06b5402504532c
spooner77 <joker.87.06@gmail.com>
2015-11-13 11:07:39 +0600

    fix trouble with sending email

--- ow_system_plugins/base/controllers/ajax_send_message_to_email.php
+++ ow_system_plugins/base/controllers/ajax_send_message_to_email.php
@@ -66,7 +66,7 @@ class BASE_CTRL_AjaxSendMessageToEmail extends OW_ActionController
         }
 
         $mail = OW::getMailer()->createMail();
-        $mail->setSender(OW::getUser()->getEmail());
+        $mail->setSender($user->getEmail());
         $mail->setSubject($subject);
         $mail->setHtmlContent($message);
 
