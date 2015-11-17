197c9bade12681527c10ef8df8afb76562dd514e
Sergey Pryadkin <GiperProger@gmail.com>
2015-11-17 15:58:32 +0600

    add geotargening tables to core

---
+++ ow_updates/updates/9943/update.php
@@ -0,0 +1,36 @@
+<?php
+
+$dbPrefix = OW_DB_PREFIX;
+$sql = array();
+
+$sql[] =
+    "CREATE TABLE IF NOT EXISTS `{$dbPrefix}geolocationdata_ipv4` (
+  `ipFrom` varchar(200) NOT NULL,
+  `IpTo` varchar(200) DEFAULT NULL,
+  `cc2` varchar(200) NOT NULL,
+  `cc3` varchar(200) DEFAULT NULL,
+  `name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+
+$sql[] = "CREATE TABLE IF NOT EXISTS `{$dbPrefix}base_geolocation_country` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cc2` char(2) NOT NULL,
+  `cc3` char(3) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=239 DEFAULT CHARSET=utf8;";
+
+
+foreach ( $sql as $q )
+{
+    try
+    {
+        OW::getDbo()->query( $q );
+    }
+    catch (Exception $ex)
+    {
+        // Log
+    }
+}
+
