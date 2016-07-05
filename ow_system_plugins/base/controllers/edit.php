04265efc8f866314cdd485bc7a47e289585fd1b7
sergarr <arrserg@gmail.com>
2016-07-05 12:38:28 +0600

    Save unchanged profile. Avatar bugfix

--- ow_system_plugins/base/controllers/edit.php
+++ ow_system_plugins/base/controllers/edit.php
@@ -350,7 +350,7 @@ class BASE_CTRL_Edit extends OW_ActionController
                             BOL_AvatarService::getInstance()->deleteUserAvatar($userId);
                         }
                     }
-                    else
+                    elseif(isset($_POST['avatarUpdated']) && $_POST['avatarUpdated'] == 1)
                     {
                         // update user avatar
                         BOL_AvatarService::getInstance()->createAvatar($userId);
