6b625eeb62276038a4503ca35fa8d82428508f41
Azazel <a_z_a_z_e_l@bk.ru>
2016-07-20 14:03:14 +0300

    Add getClassInstance for ADMIN_CMP_UserList

--- ow_system_plugins/admin/controllers/users.php
+++ ow_system_plugins/admin/controllers/users.php
@@ -204,7 +204,7 @@ class ADMIN_CTRL_Users extends ADMIN_CTRL_Abstract
                 break;
         }
         
-        $usersCmp = new ADMIN_CMP_UserList($par);
+        $usersCmp = OW::getClassInstance("ADMIN_CMP_UserList", $par);
         $this->addComponent('userList', $usersCmp);
 
         if ( !OW::getRequest()->isAjax() )
