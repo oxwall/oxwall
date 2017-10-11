3f4c4980e35b009910c1584c88c70b0450331d30
esase <alexermashev@gmail.com>
2017-10-11 16:56:08 +0600

    add language switcher in mobile mode

--- ow_static/plugins/base/js/mobile.js
+++ ow_static/plugins/base/js/mobile.js
@@ -128,6 +128,32 @@ var OWMobile = function(){
 //        }
 //    );
 
+    this.addUrlParam = function(key, value, sourceURL) {
+        sourceURL += sourceURL.indexOf("?") !== -1
+            ? '&' + key + '=' +  value
+            : '?' + key + '=' +  value;
+
+        return sourceURL;
+    }
+
+    this.removeUrlParam = function(key, sourceURL) {
+        var rtn = sourceURL.split("?")[0],
+            param,
+            params_arr = [],
+            queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+        if (queryString !== "") {
+            params_arr = queryString.split("&");
+            for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+                param = params_arr[i].split("=")[0];
+                if (param === key) {
+                    params_arr.splice(i, 1);
+                }
+            }
+            rtn = rtn + "?" + params_arr.join("&");
+        }
+        return rtn;
+    }
+
     this.escapeRegExp = function(s) {
         return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
     }
