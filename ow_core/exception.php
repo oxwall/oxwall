4fb892353f88fd91d981408d5f56dcdfd9aa10aa
Nazar <nazar.attention@gmail.com>
2023-10-13 00:02:32 +0600

    add changes for fake/console

--- ow_core/exception.php
+++ ow_core/exception.php
@@ -248,3 +248,10 @@ class ApiAccessException extends ApiResponseErrorException
         ));
     }
 }
+
+class ApiException extends Exception
+{
+    public function __construct($message = '', $code = 0) {
+        parent::__construct($message, $code);
+    }
+}
